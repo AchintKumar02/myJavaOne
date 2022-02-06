ddc382ec0ef4374385310f68ab4b8efb03babed7
Achint <kumarachint22@gmail.com>
2022-02-06 14:44:38 +0530

    JAVA

---
+++ Example2.java
@@ -0,0 +1,10 @@
+class Example2 {
+    public static void main (String args[]){
+        int num;
+        num = 100;
+        System.out.println("This is the num " + num);
+        num = num * 2;
+        System.out.println("The value of num is : ");
+        System.out.println(num);
+    }
+}
